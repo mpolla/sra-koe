e8a31331933bb83a63655726676e36d80b6545b4
Matti Pöllä <mpo@iki.fi>
2025-01-30 23:31:09 +0200

    Cypress: Oletusresoluttiona tavallinen Android-luuri

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,5 +4,8 @@ export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:4173/sra-ampumakoe-laskuri/'
-  }
+  },
+  // Perusluuri Samsung Galaxy A52 (2022)
+  viewportHeight: 915,
+  viewportWidth: 412
 })
